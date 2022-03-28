4202a1487d160bfa25df13991bcd7464107e8b2f
martin <martin.imbastari98@gmail.com>
2022-03-28 17:58:58 -0300

    homework 1

--- FT-M1-master/00-IntroToCS/homework/homework.js
+++ FT-M1-master/00-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let suma = 0;
+  for (let i = 0; i < num.length; i++) {
+    suma += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return suma;
 }
+
+//'10100'
+//0 -> 1 2 ** (4 - 0) 2**4 = 16;
+//0 -> 0 2 ** (4 - 1) 2**3 = 8; (no cuenta porque num[i] vale 0
+
+    //opcion con expresion regular
+    //num = num.replace(/[^01]/gi, '');
+    //return Number.parseInt(num, 2);
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let number = parseInt(num);
+  let resultado = number.toString(2);
+	return resultado;
 }
 
 
