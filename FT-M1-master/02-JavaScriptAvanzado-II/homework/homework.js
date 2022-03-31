d8fac1f64a721d240deb13212614180b334eb421
Martín Imbastari <71024069+martinimbastari@users.noreply.github.com>
2022-03-31 12:04:26 -0300

    hm ready

--- FT-M1-master/02-JavaScriptAvanzado-II/homework/homework.js
+++ FT-M1-master/02-JavaScriptAvanzado-II/homework/homework.js
@@ -7,7 +7,13 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+var valor = 1;
+return function() {
+  return valor++;
 }
+
+}
+
 
 function cacheFunction(cb) {
   // Usa closures para crear un caché para la función cb.
@@ -21,6 +27,14 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  var obj = {}
+  return function(random) {
+    if (obj.hasOwnProperty(random)) {
+      return obj[random]
+    }
+     obj[random] = cb(random);
+     return obj[random];
+  }
 }
 
 // Bind
@@ -35,14 +49,15 @@ var alumno = {
   curso: "FullStack"
 }
 
-function getNombre(){
+  function getNombre(){
   return this.nombre;
 }
+
  // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
@@ -61,11 +76,11 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind(undefined, '*', '*');
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind(null, '-', '-');
 
-let textoUnderscore = undefined;
+let textoUnderscore = crearCadena.bind(this, '_', '_');
 
 
 
